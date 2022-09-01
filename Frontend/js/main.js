a456e5bd77e90dc03c1eec11f85e39a74d3fb11f
Ahmed Amin <ahmedamin4449@gmail.com>
2022-09-01 13:35:45 +0200

    finish

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -6,8 +6,8 @@ var productsContainer = [];
 
 
 
+function getData(){
 
-function fetchData(){
   fetch(`http://localhost:3000/getAllProducts`)
     .then(response => response.json())
     .then(json =>  {
@@ -15,10 +15,9 @@ function fetchData(){
       productsContainer = json.result
       displayProducts();
     }
-    );
+    )
 }
-
-fetchData();
+getData();
 
 function addProduct() {
   if(productNameInput.value === ''){
@@ -34,17 +33,30 @@ function addProduct() {
     document.getElementById('alertDes').style.display = 'none';
 
     var product = {
-      "name": productNameInput.value,
-      "price": productPriceInput.value,
-      "description": productDescInput.value
+      name: productNameInput.value,
+      price: productPriceInput.value,
+      description: productDescInput.value
   }
   productsContainer.push(product);//1;
   reqServer("POST",'addProduct',product);
   clearForm();
-  fetchData();
   }
   
         
+}
+function displayProducts() { 
+  let cartoona = ``;
+    for(let i =0;i<productsContainer.length ;i++)
+    {
+        cartoona +=`<tr>
+        <td>${productsContainer[i].name}</td>
+        <td>${productsContainer[i].price}</td>
+        <td>${productsContainer[i].description}</td>
+        <td> <button onclick="retrive(${i})" class="btn btn-outline-warning">update</button> </td>
+        <td> <button onclick="deleteProduct(${productsContainer[i].id})" class="btn btn-outline-danger">delete</button> </td>
+    </tr>`
+    }
+    document.getElementById('tableBody').innerHTML =cartoona;
 }
 
 function clearForm() {
@@ -54,34 +66,20 @@ function clearForm() {
     productDescInput.value = "";
   }
 
-function displayProducts() { 
-    let cartoona = ``;
-      for(let i =0;i<productsContainer.length ;i++)
-      {
-          cartoona +=`<tr>
-          <td>${productsContainer[i].name}</td>
-          <td>${productsContainer[i].price}</td>
-          <td>${productsContainer[i].description}</td>
-          <td> <button onclick="retrive(${i})" class="btn btn-outline-warning">update</button> </td>
-          <td> <button onclick="deleteProduct(${productsContainer[i].id})" class="btn btn-outline-danger">delete</button> </td>
-      </tr>`
-      }
-      document.getElementById('tableBody').innerHTML =cartoona;
- }
 
 
 
  function deleteProduct(id){
+  // alert(id);
   let product = {
     id
   }
   reqServer("DELETE",'delete',product);
-  fetchData();
  }
 
-let id;
+let idP;
 function retrive(idx){
-  id = idx;
+  idP = productsContainer[idx].id;
   productNameInput.value = productsContainer[idx].name;
   productPriceInput.value = productsContainer[idx].price;
   productDescInput.value = productsContainer[idx].description;
@@ -95,14 +93,12 @@ document.getElementById('add').style.display = 'none';
 
 function updateProduct(){
   var product = {
-    "id":productsContainer[id].id,
+    "id":idP,
     "name": productNameInput.value,
     "price": productPriceInput.value,
     "description": productDescInput.value
 }
-
   reqServer("PUT",'update',product);
-  fetchData();
   document.getElementById('update').style.display = 'none';
   document.getElementById('add').style.display = 'block';
   clearForm();
@@ -112,15 +108,33 @@ function updateProduct(){
 
 
 function reqServer(method, endPoint, data){
-  const URL = `http://localhost:3000/${endPoint}`
-  // Send a post request
-  fetch(URL, {
-     method: method,
-     body: JSON.stringify(data),
-     headers: {
-        "Content-type": "application/json; charset=UTF-8"
-     }
-  }).then(fetchData());
+  // main.js
+
+// POST request using fetch()
+fetch(`http://localhost:3000/${endPoint}`, {
+	
+	// Adding method type
+	method: method,
+	
+	// Adding body or contents to send
+	body: JSON.stringify(data),
+	
+	// Adding headers to the request
+	headers: {
+		"Content-type": "application/json; charset=UTF-8"
+	}
+})
+
+// Converting to JSON
+.then(response => response.json())
+
+// Displaying results to console
+.then(json => {
+  if(json.message == 'success'){
+    getData();
+  }
+});
+
 
 }
 
@@ -142,3 +156,22 @@ function searchProduct(word){
   document.getElementById('tableBody').innerHTML =cartoona;
 
 }
+
+
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 5,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
